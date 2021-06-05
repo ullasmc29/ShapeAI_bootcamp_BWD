a3728259069a3330ddbb92d76dd794c94582b0c0
ullasmc29 <83940712+ullasmc29@users.noreply.github.com>
2021-06-05 14:41:10 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <h1>SHAPE AI TEAM THANKS </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>THANKS FOR THE BOOTCAMP</h1>
+      <p>
+        It is so good Bootcamp In 7 days I covered
+        everything from Scratch including Javascript React.js, HTML.
+      </p>
+    </div>
+  );
+}
+
+export default Note;
